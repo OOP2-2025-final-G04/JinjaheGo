b74b4e6493c225bd11b31c30c33dabb9e2450883
あらた <136658330+arata-06@users.noreply.github.com>
2026-01-06 23:06:01 +0900

    他画面に飛ぶための選択するボタンを作ったよ
    初期画面はapp.pyと同じだからURLをhttp://127.0.0.1:8080/chooseにしてやってみて

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 from models.models import initialize_database
+from routes.choose import choose_bp
 
 app = Flask(__name__)
 
 initialize_database()
 
+app.register_blueprint(choose_bp)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/choose")
+def choose():
+    points = 0  # 仮のポイント値
+    return render_template("choose.html", points=points)
 
 
 if __name__ == "__main__":
